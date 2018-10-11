24edf715fb448e02354cf15aa399c42084b25d19
ViktorOnegin <33310165+ViktorOnegin@users.noreply.github.com>
2018-10-11 09:26:50 +0300

    xd

---
+++ ListViewExample/ListViewExample/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace ListViewExample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        string[] countries = new string[] { "Eesti", "Soome", "Rootsi" };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.activity_main);
+
+            ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleExpandableListItem1, countries);
+            ListView.ItemClick += ListView_ItemClick;
+
+        }
+
+        public void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs args)
+        {
+            Toast.MakeText(Application, ((TextView)args.View).Text, ToastLength.Long).Show();
+        }
+    }
+}
